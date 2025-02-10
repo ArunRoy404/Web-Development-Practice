70aceb4b2dcf78642e0c48b4b3ba489f27c6316d
ArunRoy404 <arunroy0065@gmail.com>
2025-02-10 22:22:31 +0600

    upto module-8 done

---
+++ Milestone-4/Module-17/practice/average.js
@@ -0,0 +1,3 @@
+var i = prompt()
+
+console.log(i)
---
+++ Milestone-4/Module-17/practice/calculate_return.js
@@ -0,0 +1,7 @@
+var money = 1000
+var mangoPrice = 100
+var fishPrice = 200
+
+totalExpense = 1000 - (mangoPrice*2 + fishPrice*3)
+
+console.log(totalExpense)
